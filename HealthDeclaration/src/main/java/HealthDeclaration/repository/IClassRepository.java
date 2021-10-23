5a3d07ab76f150b6a0754f8b4ac24ace2bc97648
hipe <hiepkarate@gmail.com>
2021-10-23 16:50:33 +0700

    commit

--- HealthDeclaration/src/main/java/HealthDeclaration/repository/IClassRepository.java
+++ HealthDeclaration/src/main/java/HealthDeclaration/repository/IClassRepository.java
@@ -14,10 +14,4 @@ import java.util.List;
 
 @Repository
 public interface IClassRepository extends JpaRepository<Class, Long>, CrudRepository<Class, Long> {
-
-//    @Query(value = "SELECT new HealthDeclaration.modal.dto.ClassDto(cl.id, cl.name, acc.id, acc.username, acc.fullName)" +
-//            " from Class cl JOIN Account acc ON acc.id = cl.teacherId " +
-//            " where cl.name like :className and (acc.username like :teacherName or acc.fullName like :teacherName) " +
-//            " ORDER BY cl.name ASC ")
-//    List<ClassDto> searchClass(@Param("className") String className,@Param("teacherName")  String teacherName, Pageable pageable);
 }
