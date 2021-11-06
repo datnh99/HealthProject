dba282ddd22a814388ef74b732e6b33f1008a357
hipe <hiepkarate@gmail.com>
2021-11-06 18:59:42 +0700

    update

--- Front-End/src/config/preloader.js
+++ Front-End/src/config/preloader.js
@@ -1,19 +1,19 @@
 export function preloaderFinished() {
-    const body = document.querySelector('body');
-    const preloader = document.querySelector('.preloader');
-    if (!body) {
-        return;
-    }
+  const body = document.querySelector("body");
+  const preloader = document.querySelector(".preloader");
+  if (!body) {
+    return;
+  }
 
-    setTimeout(() => {
-        if (!preloader) {
-            return;
-        }
-        preloader.addEventListener('transitionend', () => {
-            preloader.className = 'preloader-hidden';
-        });
-        preloader.className += ' preloader-hidden-add preloader-hidden-add-active';
-        body.style.overflow = '';
-        body.style.overflow = '';
-    }, 100);
+  setTimeout(() => {
+    if (!preloader) {
+      return;
+    }
+    preloader.addEventListener("transitionend", () => {
+      preloader.className = "preloader-hidden";
+    });
+    preloader.className += " preloader-hidden-add preloader-hidden-add-active";
+    body.style.overflow = "";
+    body.style.overflow = "";
+  }, 100);
 }
