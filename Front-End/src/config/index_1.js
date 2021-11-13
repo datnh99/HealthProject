016b22f5013649dddd859a66d10ee1efe0ef7062
duynd0909 <72845301+duynd0909@users.noreply.github.com>
2021-11-14 01:13:41 +0700

    fix

---
+++ Front-End/src/config/index_1.js
@@ -0,0 +1,22 @@
+const config = {
+  apiUrl: "https://back-end-health.herokuapp.com",
+  CLIENT_URL: "https://front-end-health.herokuapp.com",
+  SCREEN_CODE: {
+    ROLE_MANAGEMENT: "ROLE_MANAGEMENT",
+    ADMIN_ACTION: "ADMIN_ACTION",
+    USER_MANAGEMENT: "USER_MANAGEMENT",
+    CLASS_MANAGEMENT: "CLASS_MANAGEMENT",
+    SYSTEM_CONFIG: "SYSTEM_CONFIG",
+  },
+  PERMISSION_SCREEN_MAP: {
+    UserRoleManagement: "ROLE_MANAGEMENT",
+  },
+  LOGIN_URL: "https://front-end-health.herokuapp.com/authenticate",
+  DISPLAY_SCREEN_MAP: {
+    ADMIN_ACTION: "HIEU_TRUONG,HIEU_PHO,GIAO_VIEN_CHU_NHIEM",
+    USER_MANAGEMENT: "HIEU_TRUONG,HIEU_PHO,GIAO_VIEN_CHU_NHIEM",
+    CLASS_MANAGEMENT: "HIEU_TRUONG,HIEU_PHO,GIAO_VIEN_CHU_NHIEM",
+    SYSTEM_CONFIG: "HIEU_TRUONG",
+  },
+};
+export default config;
